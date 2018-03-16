839bbb8067a2b5652591e1abbc6106608e6467ac
Valden <valden219@gmail.com>
2018-03-16 17:20:38 +0200

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-mxfz3(9z-0xnlvk-pnv=_%q5xmqxsqaj8&dx1)7ra&4if-(4o'
+SECRET_KEY = 't03(j0+m=-iomqy=tcb0j98ozo1mbv*)0jyqvyuq=il1)!kpb&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','Valden.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'Valden.pythonanywhere.com']
+
 
 
 # Application definition
